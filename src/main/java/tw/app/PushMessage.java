21c850912e1cef1c53c07b3a080608100ab47891
vanchhay <vanchhay>
2018-06-24 11:57:22 +0700

    Updated: Create PushMessage class to handle PushMessage deserialization from Webservice

---
+++ src/main/java/tw/app/PushMessage.java
@@ -0,0 +1,50 @@
+package tw.app;
+
+import java.time.Instant;
+
+public class PushMessage {
+
+	String sender, topic, text;
+	boolean urgent;
+	Instant sendTime;
+
+	public void setSender (String sender) {
+		this.sender = sender;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void setUrgent(boolean urgent) {
+		this.urgent = urgent;
+	}
+
+	public void setSendTime(Instant sendTime){
+		this.sendTime = sendTime;
+	}
+
+	public String getSender(){
+		return this.sender;
+	}
+
+	public String getTopic(){
+		return this.topic;
+	}
+
+	public String getText(){
+		return this.text;
+	}
+
+	public boolean getUrgent(){
+		return this.urgent;
+	}
+
+	public Instant getSendTime(){
+		return this.sendTime;
+	}
+}
--- src/main/java/tw/app/PushMessageDistributorApplication.java
+++ src/main/java/tw/app/PushMessageDistributorApplication.java
@@ -1,9 +1,6 @@
 package tw.app;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import org.apache.kafka.clients.consumer.*;
 
 import org.apache.kafka.common.serialization.LongDeserializer;
@@ -14,7 +11,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.time.Instant;
 import java.util.Collections;
 import java.util.Properties;
 
@@ -58,29 +54,13 @@ public class PushMessageDistributorApplication {
 
 		// Listen to consumer for coming message to log
 		Gson gson = new Gson();
-		JsonParser parser = new JsonParser();
-		String originData, topic, sender, text;
-		boolean urgent;
-		JsonElement deData;
-
 		while (true) {
 			final ConsumerRecords<Long, String> consumerRecords = consumer.poll(10);
+			PushMessage pm;
 			for (ConsumerRecord<Long, String> record : consumerRecords) {
-				originData = record.value();
-				deData = parser.parse(originData);
-
-				if(deData.isJsonObject()) {
-					JsonObject jsonObj = deData.getAsJsonObject();
-					// Data Assignment
-					topic = gson.fromJson(jsonObj.get("topic"), String.class);
-					sender = gson.fromJson(jsonObj.get("sender"), String.class);
-					text = gson.fromJson(jsonObj.get("text"), String.class);
-					urgent = gson.fromJson(jsonObj.get("urgent"), boolean.class);
-					Instant sendTime = gson.fromJson(jsonObj.get("sentTime"), Instant.class);
-
-					LOGGER.info("[{} - PushMessage : Sender: {} , Topic: {} , Urgent: {} , Text: {} ",
-							sendTime, sender, topic, urgent, text);
-				}
+					pm = gson.fromJson(record.value(), PushMessage.class);
+					LOGGER.info("[{}] - PushMessage : Sender: {} , Topic: {} , Urgent: {} , Text: {}",
+							pm.getSendTime(), pm.getSender(), pm.getTopic(), pm.getUrgent(), pm.getText());
 			}
 			consumer.commitAsync();
 		}
